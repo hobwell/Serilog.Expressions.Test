b44919bb20c32e98947ad3f9a0190be7af139444
hobwell <11163233+hobwell@users.noreply.github.com>
2022-09-13 11:29:36 -0400

    Add project files.

---
+++ SerilogExpressionsTestCore/Enricher.cs
@@ -0,0 +1,28 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace SerilogExpressionsTestCore
+{
+    /// <summary>
+    /// Basic enricher, not desctucturing when adding a property
+    /// </summary>
+    internal class Enricher : ILogEventEnricher
+    {
+
+        private static Dictionary<string, string> QueryString
+        {
+            get
+            {
+                return new Dictionary<string, string>() {
+                    { "x", "NULL" },
+                    { "y", "DROP" }
+                };
+            }
+        }
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("QueryString", QueryString));
+        }
+    }
+}
---
+++ SerilogExpressionsTestCore/Program.cs
@@ -0,0 +1,34 @@
+﻿using Serilog;
+using Serilog.Core;
+using Serilog.Templates;
+using SerilogExpressionsTestCore;
+namespace SerilogExpressionTest
+{
+    class Program
+    {
+
+        private static Logger? Logger;
+
+        static void Main(string[] args)
+        {
+            Logger = GetLogger();
+            Logger.Error("Test");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Initialize the Logger with a new Logger configuration
+        /// </summary>
+        private static Logger GetLogger()
+        {
+            LoggerConfiguration config = new LoggerConfiguration()
+            .Enrich.With(new Enricher())
+            .WriteTo.Console(new ExpressionTemplate("QueryString[?] like '%DROP%' ci: {#if QueryString[?] like '%DROP%' ci}True{#else}False{#end}\n\n"), restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Verbose, levelSwitch: null)
+            .WriteTo.Console(new ExpressionTemplate("QueryString['y'] like '%DROP%' ci: {#if QueryString['y'] like '%DROP%' ci}True{#else}False{#end}\n\n"), restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Verbose, levelSwitch: null)
+            .WriteTo.Console(new ExpressionTemplate("{@p}\n\n"), restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Verbose, levelSwitch: null);
+            return config.CreateLogger();
+        }
+
+    }
+
+}
